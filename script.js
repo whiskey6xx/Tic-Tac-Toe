e37098939a2f8171bb5129531a6c83a8d41a79bd
whiskey6xx <wheelergrantj@gmail.com>
2021-11-13 18:31:48 -0500

    added functionality to UI.placePiece that checks to see if a marker is already in the square

--- script.js
+++ script.js
@@ -9,13 +9,20 @@ const UI = (() => {
     let gridsquare = document.querySelectorAll('.grid-square');
     marker = "X";
     const placePiece = (target) => {
-        target.innerText = marker;
+
+        if (target.innerText ==! "<empty string>") {
+            target.innerText = marker;
+        } else {
+            alert("Don't do that")
+        }
+        
         if (marker === "X") {
             marker = "O";
         } else if (marker = "O") {
             marker = "X";
-        }   
+        }  
     } 
+
     return {
         placePiece,
         gridsquare
@@ -27,6 +34,8 @@ const UI = (() => {
 gridsquare = UI.gridsquare;
 gridsquare.forEach(square => {
     square.addEventListener('click', () => {
+        console.log(square.innerText);
         UI.placePiece(square);
+        console.log(square.innerText);
     });
 });
